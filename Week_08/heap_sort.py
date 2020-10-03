b2529e8300143d8c723094646e26ad1bd092ee07
frank <frank.chang@shoufuyou.com>
2020-10-03 16:48:29 +0800

    add  heap sort code

--- Week_08/heap_sort.py
+++ Week_08/heap_sort.py
@@ -1,4 +1,32 @@
+from heapq import heappush, heappop
+from typing import List
+
+from random import shuffle
 
 
-#TODO  堆排序 代码实现
+class Solution:
 
+    def heapsort(self, array: List):
+        heap = []
+        for item in array:
+            heappush(heap, item)
+
+        # 原地修改数组
+        array[:] = [heappop(heap) for _ in range(len(heap))]
+
+
+if __name__ == '__main__':
+    times = 10000
+    for i in range(times):
+        array = list(range(20))
+
+        array.sort()
+        sorted_arr = array.copy()
+
+        shuffle(array)
+
+        # print(array)
+        Solution().heapsort(array)
+
+        assert sorted_arr == array
+        print(f"{i} assert True")
